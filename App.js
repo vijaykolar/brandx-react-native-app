3ff1d37182fc0ab1e374dbd5d4804773c2ff0f74
Vijay Kolar <vijayikolar@gmail.com>
2023-09-17 21:10:51 +0530

    project init

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import { useEffect, useState } from "react";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => res.json())
+      .then((data) => setTodos(data));
+  }, []);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+      <View style={styles.container}>
+        <Text style={styles.text}>This is sample text up</Text>
+        {todos.map((todo) => (
+          <Text style={styles.todo} key={todo.id}>
+            {todo.title}
+          </Text>
+        ))}
+        <Text>{todos.length}</Text>
+
+        <StatusBar style="auto" />
+      </View>
+    </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fffcfc",
+    alignItems: "center",
+    justifyContent: "center",
+    paddingLeft: 4,
+    paddingRight: 4,
+  },
+  text: {
+    color: "#22ff00",
+    fontSize: 40,
+    fontWeight: "900",
+  },
+  todo: {
+    padding: 2,
   },
 });
